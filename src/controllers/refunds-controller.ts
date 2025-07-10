d241014d98d4427803e2ba38263bd4ef764a6d2b
“Joiran <joiran.costa.jc@gmail.com>
2025-07-10 17:51:56 -0300

    add refunds

---
+++ src/controllers/refunds-controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express"
+
+class RefundsController {
+  async create(request: Request, response: Response){
+
+    response.json({ message: "ok" })
+  }
+}
+
+export { RefundsController }
